585d77d2092ddd443144745c53041ca597a4dfe9
hastinbara <tinhasbara@gmail.com>
2025-08-11 09:48:19 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // src/main.js
-import { createApp } from 'vue'
-import { Quasar } from 'quasar'
-import App from './App.vue'
+import { createApp } from "vue";
+import { Quasar } from "quasar";
+import router from "./router";
+import App from "./App.vue";
 
 // Estilos do Quasar e Tailwind
-import 'quasar/src/css/index.sass'
-import './assets/tailwind.css'
+import "quasar/src/css/index.sass";
+import "./assets/tailwind.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(Quasar, {
-  plugins: {} // Adicione plugins do Quasar aqui se precisar
-})
+app.use(router);
+app.use(Quasar, { plugins: {} });
 
-app.mount('#app')
+app.mount("#app");
